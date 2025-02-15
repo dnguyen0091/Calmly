a5255a350f452d88072a0bc876f576068bc5605b
David Nguyen <107872059+dnguyen0091@users.noreply.github.com>
2025-02-15 18:05:18 -0500

    Add files via upload
    added close feature for form

--- src/components/SignInUp/signInUp.jsx
+++ src/components/SignInUp/signInUp.jsx
@@ -1,40 +1,38 @@
+import React from 'react';
+import '../../index.css';
 import Login from './login.jsx';
 import Register from './register.jsx';
 
-export default function SignInUp() {
+export default function SignInUp({ onClose }) {
+    const [mode, setMode] = React.useState('signup');
 
-    const sliderHandler = (mode) => {
-        console.log(`${mode} button clicked`);
-        // Update state logic to render the appropriate form based on mode
-
-        if (mode==='signin'){
-            <Login />
-        }
-        else{
-            <Register />
-        }
+    const sliderHandler = (selectedMode) => {
+        console.log(`${selectedMode} button clicked`);
+        setMode(selectedMode);
     };
-    return(
 
-        
-        // overlay  background
-        <div className="formOverlay absolute top-0 left-0 z-50 h-[100vh] w-[100vw] bg-black/75 flex justify-center items-center">
-            
-            <div className="bg-white p-6 rounded shadow-xl">
-                {/* Form */}
-                <div className="form flex flex-col justify-center items-center align-center gap-[10vh]">
-                    {/* Slider for form */}
-                    <div className="sliderContainer gap-0 h-[7vh] w-[15vw]">
-                        <button onClick={() => sliderHandler('signup')}>Sign Up</button>
-                        <button onClick={() => sliderHandler('signin')}>Sign In</button>
-                    </div>
+    const closeHandler = () => {
+        console.log('close button clicked');
+        onClose();
+    };
 
+    return (
+        // overlay background
+        <div className="formOverlay fixed top-0 left-0 z-50 h-[100vh] w-[100vw] bg-black/75 flex justify-center items-center">
+        <div className="bg-[var(--primaryColor)] relative rounded shadow-xl w-[25vw] min-h-[50vh] flex flex-col justify-center items-center text-center">
+            {/* Form */}
+            <div className="form flex flex-col justify-center items-center">
+                {/* Slider for form */}
+                <button onClick={closeHandler} className="absolute right-0 top-0 p-[0.5vw]">X</button>
+                <div className="sliderContainer absolute top-0 flex flex-row justify-center gap-0 h-[7vh] w-[15vw] text-black">
+                    <button onClick={() => sliderHandler('signup')}>Sign Up</button>
+                    <button onClick={() => sliderHandler('signin')}>Sign In</button>
                     
                 </div>
+
+            {mode === 'signin' ? <Login /> : <Register />}
             </div>
         </div>
-        
-    )
+        </div>
+    );
 }
-
-
