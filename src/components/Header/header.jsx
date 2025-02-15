e5404b021ff404bf3b00d7a96dfd2111557f6d6d
David Nguyen <107872059+dnguyen0091@users.noreply.github.com>
2025-02-15 17:21:33 -0500

    Add files via upload
    recolored header for new scheme

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -12,14 +12,14 @@ export default function Header() {
   };
 
   return (
-    <div className="header bg-[var(--primaryColor)] h-[10vh] text-[var(--textColor)] flex justify-center items-center px-4 relative">
+    <div className="header bg-white h-[10vh] text-[var(--textColor)] flex justify-center items-center px-4 relative shadow-xl">
       <img src="" alt="Logo Placeholder" />
-      <div className="absolute right-0">
+      <div className="absolute right-0 p-[2vw] ">
         <button
           onClick={handleSignInClick}
-          className="signInUpButton flex justify-center items-center cursor-pointer border border-[var(--borderColor)] overflow-hidden relative m-0 p-[17px_35px] outline-none no-underline uppercase"
+          className="signInUpButton bg-[var(--secondaryColor)] flex justify-center items-center cursor-pointer border border-[var(--borderColor)] overflow-hidden relative m-0 p-[17px_35px] outline-none no-underline uppercase shadow-xl"
         >
-          Sign Up
+          <p className="text-white ">Sign Up</p>
         </button>
       </div>
       {showSignInUp && <SignInUp />}
