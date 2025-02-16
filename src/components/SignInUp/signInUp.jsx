349975745c612c512534212afdad028a4c12cb04
David Nguyen <107872059+dnguyen0091@users.noreply.github.com>
2025-02-16 10:54:02 -0500

    Add files via upload
    Added slider to toggle between sign in/up

--- src/components/SignInUp/signInUp.jsx
+++ src/components/SignInUp/signInUp.jsx
@@ -9,6 +9,13 @@ export default function SignInUp({ onClose }) {
     const sliderHandler = (selectedMode) => {
         console.log(`${selectedMode} button clicked`);
         setMode(selectedMode);
+
+        if (selectedMode === 'signin') {
+            document.querySelector('.formSlider').style.left = '50%';
+        }
+        else {
+            document.querySelector('.formSlider').style.left = '0%';
+        }
     };
 
     const closeHandler = () => {
@@ -19,20 +26,21 @@ export default function SignInUp({ onClose }) {
     return (
         // overlay background
         <div className="formOverlay fixed top-0 left-0 z-50 h-[100vh] w-[100vw] bg-black/75 flex justify-center items-center">
-        <div className="bg-[var(--primaryColor)] relative rounded shadow-xl w-[25vw] min-h-[50vh] flex flex-col justify-center items-center text-center">
-            {/* Form */}
-            <div className="form flex flex-col justify-center items-center">
-                {/* Slider for form */}
-                <button onClick={closeHandler} className="absolute right-0 top-0 p-[0.5vw]">X</button>
-                <div className="sliderContainer absolute top-0 flex flex-row justify-center gap-0 h-[7vh] w-[15vw] text-black">
-                    <button onClick={() => sliderHandler('signup')}>Sign Up</button>
-                    <button onClick={() => sliderHandler('signin')}>Sign In</button>
-                    
-                </div>
+            <div className="bg-[var(--primaryColor)] relative rounded shadow-xl w-[25vw] min-h-[50vh] flex flex-col justify-center items-center text-center p-[1vh]">
+                {/* Form */}
+                <div className="form flex flex-col justify-center items-center">
+                    {/* Slider for form */}
+                    <button onClick={closeHandler} className="absolute right-0 top-0 p-[0.5vw]">X</button>
+                    <div className="sliderContainer absolute top-0 flex flex-row justify-center gap-0 w-[10.5vw] mt-[3vh] border-black border-[0.1rem] rounded-4xl text-black">
+                        <div className="formSlider absolute top-0 left-0 w-[50%] h-[100%] bg-[var(--backgroundColor)] rounded-4xl transition-[0.4s]"></div>
+                        <button onClick={() => sliderHandler('signup')} className="w-[50%] p-[10px] cursor-pointer bg-transparent border-0 outline-none relative text-[var(--textColor)]">Sign Up</button>
+                        <button onClick={() => sliderHandler('signin')} className="w-[50%] p-[10px] cursor-pointer bg-transparent border-0 outline-none relative text-[var(--textColor)]">Sign In</button>
+                        
+                    </div>
 
-            {mode === 'signin' ? <Login /> : <Register />}
+                {mode === 'signin' ? <Login /> : <Register />}
+                </div>
             </div>
-        </div>
         </div>
     );
 }
