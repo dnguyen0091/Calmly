a5b6c0db231f9b3f88676d54a0005e12c74ed1e2
David Nguyen <107872059+dnguyen0091@users.noreply.github.com>
2025-02-16 20:55:17 -0500

    Add files via upload
    design changes for register

--- src/components/SignInUp/register.jsx
+++ src/components/SignInUp/register.jsx
@@ -3,17 +3,24 @@ export default function Register() {
     return(
         // form input fields for registration
 
-        <div>
-            <input type="text" placeholder="Name" />
-            <input type="text" placeholder="email" />
-            <input type="password" placeholder="password" />
-            <input type="password" placeholder="confirm password" />
-            <button>Sign Up</button>
+        <div className="form flex flex-col justify-center items-center gap-[1rem]">
+            <div className="flex flex-row gap-[1rem]">
+                <input type="text" placeholder="Name" className="w-[9vw] h-[4vh] bg-white border-[var(--backgroundColor)] placeholder:text-[var(--secondaryColor)] text-[var(--secondaryColor)] border-[0.2rem] p-[0.5em] rounded-xl"/>
+                <input type="text" placeholder="Email" className="w-[9vw] h-[4vh] bg-white border-[var(--backgroundColor)] placeholder:text-[var(--secondaryColor)] text-[var(--secondaryColor)] border-[0.2rem] p-[0.5em] rounded-xl"/>
+            </div>
+            
+            <input type="password" placeholder="Password" className="w-[15vw] h-[4vh] bg-white border-[var(--backgroundColor)] placeholder:text-[var(--secondaryColor)] text-[var(--secondaryColor)] border-[0.2rem] p-[0.5em] rounded-xl"/>
+            <input type="password" placeholder="Confirm Password" className="w-[15vw] h-[4vh] bg-white border-[var(--backgroundColor)] placeholder:text-[var(--secondaryColor)] text-[var(--secondaryColor)] border-[0.2rem] p-[0.5em] rounded-xl"/>
+            <button className="w-[15vw] h-[4vh] bg-transparent text-[var(--textColor)] border-[var(--backgroundColor)] border-[0.2rem] p-[0.5em] rounded-xl">Sign Up</button>
 
             {/* Add divider */}
-            <div>
-                <p>or</p>
-                <div>
+            <div className="flex items-center flex-col">
+                <div className="flex items-center w-[20vw] my-4">
+                    <div className="flex-grow border-t border-[var(--textColor)]-300"></div>
+                    <span className="mx-4 text-[var(--textColor)]-500">OR</span>
+                    <div className="flex-grow border-t border-[var(--textColor)]-300"></div>
+                </div>
+                <div className="flex flex-row gap-[2rem]">
                     <a href=""><img src="" alt="google Icon" /></a>
                     <a href=""><img src="" alt="apple Icon" /></a>
                 </div>
